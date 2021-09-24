4c954745a07a015302b8012bb1ba221c8e99a0f6
Zmeypp <90198158+Zmeypp@users.noreply.github.com>
2021-09-24 13:32:01 +0200

    Update PageConnexion.php

--- PageConnexion.php
+++ PageConnexion.php
@@ -3,29 +3,31 @@
 	include('./Connection_BDD.php');
 	$conn = getBdd('localhost', 'root', '');
 	
-		if(isset($_POST['formconnexion'])) {
-   $userconnect= htmlspecialchars($_POST['userconnect']);
-   $mdpconnect = $_POST['mdpconnect'];
-   if(!empty($userconnect) AND !empty($mdpconnect)) {
-	  $requser = $conn->prepare("select * from gsb_frais.utilisateur where login = ? and mdp = ?");
-	  $requser->execute(array($userconnect, $mdpconnect));
-	  $userexist = $requser->rowCount();
-      if($userexist == 1) {
-         $userinfo = $requser->fetch();
-         header("Location: profil.php");
-      } else {
-         $erreur = "Mauvais nom d'utilisateur ou mot de passe !";
-      }
-   } else {
-      $erreur = "Tous les champs doivent être complétés !";
-   }
+	if(isset($_POST['formconnexion'])) {
+   		$userconnect= htmlspecialchars($_POST['userconnect']);
+		$mdpconnect = $_POST['mdpconnect'];
+   		if(!empty($userconnect) AND !empty($mdpconnect)) {
+	  		$requser = $conn->prepare("select * from gsb_frais.utilisateur where login = ? and mdp = ?");
+	  		$requser->execute(array($userconnect, $mdpconnect));
+	 		$userexist = $requser->rowCount();
+      			if($userexist == 1) {
+         			$userinfo = $requser->fetch();
+         			header("Location: profil.php");
+      			} 
+			else {
+         			$erreur = "Mauvais nom d'utilisateur ou mot de passe !";
+      			}
+   		} 
+		else {
+      			$erreur = "Tous les champs doivent être complétés !";
+   		}
    
-   $reponse = $conn->query( 'SELECT idr from utilisateur WHERE login="'.$_POST['userconnect'].'" AND mdp="'.$_POST['mdpconnect'].'"');
-   $data = $reponse->fetch();
-   $_SESSION['idr'] = $data['idr'];
+   		$reponse = $conn->query( 'SELECT idr from utilisateur WHERE login="'.$_POST['userconnect'].'" AND mdp="'.$_POST['mdpconnect'].'"');
+   		$data = $reponse->fetch();
+   		$_SESSION['idr'] = $data['idr'];
    
    
-}
+	}
 
 ?>
 
@@ -45,9 +47,9 @@
             <input type="submit" name="formconnexion" value="Se connecter !" />
          </form>
          <?php
-         if(isset($erreur)) {
-            echo '<font color="red">'.$erreur."</font>";
-         }
+         	if(isset($erreur)) {
+            		echo '<font color="red">'.$erreur."</font>";
+         	}
          ?>
       </div>
    </body>
